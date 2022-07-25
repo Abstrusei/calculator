767d0505d98958ef0acef26be5ad4b7544435147
Sean <smarshallean@gmail.com>
2022-07-26 00:43:44 +1000

    Several issues in logic have been fixed. Further testing still required.

--- script.js
+++ script.js
@@ -3,37 +3,23 @@ let total = 0;
 let operandA;
 let operandB;
 let operator;
-let evaluated = false;
+let equalsPressed = false;
 
-function add(a, b) {
-    return a + b; 
-}
+function add(a, b) {return a + b; }
 
-function subtract(a, b) {
-    return a - b; 
-}
+function subtract(a, b) {return a - b; }
 
-function multiply(a, b) {
-    return a * b; 
-}
+function multiply(a, b) {return a * b; }
 
-function divide(a, b) {
-    return a/b;
-}
+function divide(a, b) {return a/b;}
 
 function operate(a, b, operator) {
-    if (operator == "+") {
-        return add(a, b);
-    }
-    if (operator == "-") {
-        return subtract(a, b);
-    }
-    if (operator == "*") {
-        return multiply(a, b);
-    }
-    if (operator == "/") {
-        return divide(a, b);
-    }
+    let result = 0;
+    if (operator == "+") result = add(a, b);
+    if (operator == "-") result = subtract(a, b);
+    if (operator == "*") result = multiply(a, b);
+    if (operator == "/") result = divide(a, b);
+    return parseFloat(result.toFixed(3));
 }
 
 function isNumeric(btn){
@@ -59,15 +45,19 @@ function resetOnscreenValue() {
 function clearCalcualtor() {
     onscreenValue = "0";
     total = 0;
-    evaluated = false;
+    equalsPressed = false;
     displayValue(onscreenValue);
     resetOperand("a");
     resetOperand("b");
     resetOperator();
 }
 
+function setOperator(a) {
+    operator = a;
+}
+
 function numericPress(a) {
-    if (!evaluated) {
+    if (!equalsPressed) {
         if(onscreenValue == "0") {
             onscreenValue = a;
         } else {
@@ -84,58 +74,49 @@ function numericPress(a) {
 }
 
 function operatorPress(pressedOperator) {
-    operator = pressedOperator;
-
-    if (!evaluated) {
-        if (!isNumeric(operandA)) {
-            // operandA hasn't been filled
-            operandA = parseInt(onscreenValue);
-            total = parseInt(onscreenValue);
-        } else if (isNumeric(operandA) && isNaN(operandB)) {
-            // operandA HAS been filled  
-            operandB = parseInt(onscreenValue);
-            total = operate(total, operandB, operator);
-            displayValue(total);
+    if (!equalsPressed) {
+        if (operator == undefined || pressedOperator == operator) {
+            setOperator(pressedOperator);
+            if (operandA == undefined) {
+                operandA = parseInt(onscreenValue);
+                total = operandA;
+            } else {
+                evaluate();
+            }
         } else {
-             // operandA AND operandB have been filled 
-            operandB = parseInt(onscreenValue);
-            total = operate(total, operandB, operator);
-            operandA = total;
-            displayValue(total);
-    
-            resetOperand("b");
+            evaluate();
+            setOperator(pressedOperator);
         }
-        // resetOnscreenValue();
-    } 
+    } else {
+        if(pressedOperator != operator) {
+            setOperator(pressedOperator);
+        }
+        equalsPressed = false;
+    }
     resetOnscreenValue();
 }
 
 function equalsPress() {
-    if(isNumeric(operandA) && isNaN(operandB)) {
-        operandB = parseInt(onscreenValue);
-        total = operate(total, operandB, operator);
-        displayValue(total);
+    evaluate();
+    equalsPressed = true;
+}
 
+function evaluate() {
+    operandB = parseInt(onscreenValue);
+    total = operate(total, operandB, operator);
+    if(isNumeric(operandA) && isNaN(operandB)) {
+        displayValue(total);
         onscreenValue = total.toString();
         operandA = total;
-        evaluated = true;
-        resetOperand("b");
     } else  {
-        // 5 + 5 + 5 
-        // if "=" pressed operandA = 15, operandB = nothing
-        operandB = parseInt(onscreenValue);
-        total = operate(total, operandB, operator);
-
         operandA =  total;
         displayValue(total);
-
-        resetOperand("b");
     }
+    resetOperand("b");
 }
 
 function btnPress(e) {
     const btn = e.target.textContent;
-
     // NOTE: CURRENTLY LIMITING THE "LENGTH" OF OPERANDS TO 4 E.G. 1000
     if (isNumeric(btn)) {
         if (onscreenValue.length < 5) {
@@ -144,7 +125,7 @@ function btnPress(e) {
     } else if (btn == "AC") {
         clearCalcualtor();
     } else if (btn == "=") {
-        equalsPress();
+        if (isNumeric(operandA)) equalsPress();
     }  else if (btn == ".") {
         alert(btn + " was pressed");
     } else {
